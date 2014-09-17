5425ee523e6dcf373bd83bfc4faf6966098e7778
Ben Nortier <ben@bjnortier.com>
2014-09-17 11:41:55 +0100

    Cross product and toString() functions

--- test/vec2.test.js
+++ test/vec2.test.js
@@ -3,23 +3,25 @@ var assert = chai.assert;
 
 var Vec2 = require('..').Vec2;
 
-// For easier deepEqual comparison
-function toObj(v) {
-  return {x: v.x, y: v.y};
-}
+// Handy assert for Vec2 deepEqual
+assert.v2Equal = function(actual, expected) {
+  assert.deepEqual({x: actual.x, y: actual.y}, expected);
+};
 
 describe('Vec2', function() {
 
   it('can be constructed using an array, params or an object', function() {
 
     var va = new Vec2(3,5);
-    assert.deepEqual(toObj(va), {x: 3, y: 5});
+    assert.v2Equal(va, {x: 3, y: 5});
 
     var vb = new Vec2([-1, 7]);
-    assert.deepEqual(toObj(vb), {x: -1, y: 7});
+    assert.v2Equal(vb, {x: -1, y: 7});
 
     var vc = new Vec2({x: 98, y: -5});
-    assert.deepEqual(toObj(vc), {x: 98, y: -5});
+    assert.v2Equal(vc, {x: 98, y: -5});
+
+    assert.equal('{"x":3,"y":5}', va.toString());
 
   });
 
@@ -42,17 +44,17 @@ describe('Vec2', function() {
     var va = new Vec2(11, -7);
     var vb = new Vec2(3, 4);
 
-    assert.deepEqual(toObj(va.negate()), {x: -11, y: 7});
-    assert.deepEqual(va.dot(vb), 5);
-    assert.deepEqual(va.length(), Math.sqrt(170));
-    assert.deepEqual(toObj(vb.normalize()), {x: 3*0.2, y: 4*0.2});
-    assert.deepEqual(toObj(vb.add(va)), {x: 14, y: -3});
-    assert.deepEqual(toObj(vb.sub(va)), {x: -8, y: 11});
-    assert.deepEqual(toObj(va.sub(vb)), {x: 8, y: -11});
+    assert.v2Equal(va.negate(), {x: -11, y: 7});
+    assert.equal(va.dot(vb), 5);
+    assert.equal(va.length(), Math.sqrt(170));
+    assert.v2Equal(vb.normalize(), {x: 3*0.2, y: 4*0.2});
+    assert.v2Equal(vb.add(va), {x: 14, y: -3});
+    assert.v2Equal(vb.sub(va), {x: -8, y: 11});
+    assert.v2Equal(va.sub(vb), {x: 8, y: -11});
 
     // Original vectors are unchanged
-    assert.deepEqual(toObj(va), {x: 11, y: -7});
-    assert.deepEqual(toObj(vb), {x: 3, y: 4});
+    assert.v2Equal(va, {x: 11, y: -7});
+    assert.v2Equal(vb, {x: 3, y: 4});
 
   });
 
--- test/vec3.test.js
+++ test/vec3.test.js
@@ -3,23 +3,25 @@ var assert = chai.assert;
 
 var Vec3 = require('..').Vec3;
 
-// For easier deepEqual comparison
-function toObj(v) {
-  return {x: v.x, y: v.y, z: v.z};
-}
+// Handy assert for Vec3 deepEqual
+assert.v3Equal = function(actual, expected) {
+  assert.deepEqual({x: actual.x, y: actual.y, z: actual.z}, expected);
+};
 
 describe('Vec3', function() {
 
   it('can be constructed using an array, params or an object', function() {
 
     var va = new Vec3(3,5,7);
-    assert.deepEqual(toObj(va), {x: 3, y: 5, z: 7});
+    assert.v3Equal(va, {x: 3, y: 5, z: 7});
 
     var vb = new Vec3([-1, 7, -4]);
-    assert.deepEqual(toObj(vb), {x: -1, y: 7, z: -4});
+    assert.v3Equal(vb, {x: -1, y: 7, z: -4});
 
     var vc = new Vec3({x: 98, y: -5, z: 22});
-    assert.deepEqual(toObj(vc), {x: 98, y: -5, z: 22});
+    assert.v3Equal(vc, {x: 98, y: -5, z: 22});
+
+    assert.equal('{"x":3,"y":5,"z":7}', va.toString());
 
   });
 
@@ -46,17 +48,24 @@ describe('Vec3', function() {
     var va = new Vec3(11, -7, 13);
     var vb = new Vec3(3, 4, 5);
 
-    assert.deepEqual(toObj(va.negate()), {x: -11, y: 7, z: -13});
-    assert.deepEqual(va.dot(vb), 70);
-    assert.deepEqual(va.length(), Math.sqrt(339));
-    assert.deepEqual(toObj(vb.normalize()), {x: 3/Math.sqrt(50), y: 4/Math.sqrt(50), z: 5/Math.sqrt(50)});
-    assert.deepEqual(toObj(vb.add(va)), {x: 14, y: -3, z: 18});
-    assert.deepEqual(toObj(vb.sub(va)), {x: -8, y: 11, z: -8});
-    assert.deepEqual(toObj(va.sub(vb)), {x: 8, y: -11, z: 8});
+    assert.v3Equal(va.negate(), {x: -11, y: 7, z: -13});
+    assert.equal(va.dot(vb), 70);
+    assert.v3Equal(va.cross(vb), {x: -87, y: -16, z: 65});
+    assert.v3Equal(new Vec3(3,-3,1).cross(new Vec3(4,9,2)), 
+      {x: -15, y: -2, z: 39});
+    assert.v3Equal(new Vec3(3,-3,1).cross(new Vec3(-12,12,-4)), 
+      {x: 0, y: 0, z: 0});
+
+
+    assert.equal(va.length(), Math.sqrt(339));
+    assert.v3Equal(vb.normalize(), {x: 3/Math.sqrt(50), y: 4/Math.sqrt(50), z: 5/Math.sqrt(50)});
+    assert.v3Equal(vb.add(va), {x: 14, y: -3, z: 18});
+    assert.v3Equal(vb.sub(va), {x: -8, y: 11, z: -8});
+    assert.v3Equal(va.sub(vb), {x: 8, y: -11, z: 8});
 
     // Original vectors are unchanged
-    assert.deepEqual(toObj(va), {x: 11, y: -7, z: 13});
-    assert.deepEqual(toObj(vb), {x: 3, y: 4, z: 5});
+    assert.v3Equal(va, {x: 11, y: -7, z: 13});
+    assert.v3Equal(vb, {x: 3, y: 4, z: 5});
 
   });
 
