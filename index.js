084c61860f462d3c8f06148802d4567ced7fa5eb
Ben Nortier <ben@bjnortier.com>
2014-10-09 13:36:06 +0200

    Box3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Vec2 = require('./lib/vec2');
 var Vec3 = require('./lib/vec3');
+var Box3 = require('./lib/box3');
 var Plane3 = require('./lib/plane3');
 var Quaternion = require('./lib/quaternion');
 
 module.exports.Vec2 = Vec2;
 module.exports.Vec3 = Vec3;
+module.exports.Box3 = Box3;
 module.exports.Plane3 = Plane3;
 module.exports.Quaternion = Quaternion;
---
+++ test/box3.test.js
@@ -0,0 +1,54 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var Box3 = require('..').Box3;
+var Vec3 = require('..').Vec3;
+
+describe('Box3', function() {
+
+  it('can be expaned by a point', function() {
+
+    var p1 = new Vec3(-1,-7,-13);
+    var p2 = new Vec3(5,11,17);
+    var box = new Box3().expandByPoint(p1).expandByPoint(p2);
+
+    assert.equal(box.min.x, -1);
+    assert.equal(box.min.y, -7);
+    assert.equal(box.min.z, -13);
+    assert.equal(box.max.x, 5);
+    assert.equal(box.max.y, 11);
+    assert.equal(box.max.z, 17);
+
+  });
+
+  it('can be constructed from an array of points', function() {
+
+    var p1 = new Vec3(-1,-7,-13);
+    var p2 = new Vec3(5,11,17);
+    var box = Box3.fromPoints([p1, p2]);
+
+    assert.equal(box.min.x, -1);
+    assert.equal(box.min.y, -7);
+    assert.equal(box.min.z, -13);
+    assert.equal(box.max.x, 5);
+    assert.equal(box.max.y, 11);
+    assert.equal(box.max.z, 17);
+
+  });
+
+  it('can test whether a point lies inside it', function() {
+
+    var p1 = new Vec3(0,0,0);
+    var p2 = new Vec3(100,0,100);
+    var box = Box3.fromPoints([p1, p2]);
+
+    assert.isTrue(box.isPointInside(new Vec3(0,0,0)));
+    assert.isTrue(box.isPointInside(new Vec3(100,0,100)));
+    assert.isTrue(box.isPointInside(new Vec3(50,0,20)));
+
+    assert.isFalse(box.isPointInside(new Vec3(-1,0,0)));
+    assert.isFalse(box.isPointInside(new Vec3(101,0,0)));
+
+  });
+
+});
