4ca08f5b3488534c357232f3a46f7529c8827010
Ben Nortier <ben@bjnortier.com>
2014-10-16 14:58:13 +0200

    box2

---
+++ test/box2.test.js
@@ -0,0 +1,64 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var Box2 = require('..').Box2;
+var Vec2 = require('..').Vec2;
+
+describe('Box2', function() {
+
+  it('can be expaned by a point', function() {
+
+    var p1 = new Vec2(-1,-7);
+    var p2 = new Vec2(5,11);
+    var box = new Box2().expandByPoint(p1).expandByPoint(p2);
+
+    assert.equal(box.min.x, -1);
+    assert.equal(box.min.y, -7);
+    assert.equal(box.max.x, 5);
+    assert.equal(box.max.y, 11);
+
+  });
+
+  it('can be constructed from an array of points', function() {
+
+    var p1 = new Vec2(-1,-7);
+    var p2 = new Vec2(5,11);
+    var box = Box2.fromPoints([p1, p2]);
+
+    assert.equal(box.min.x, -1);
+    assert.equal(box.min.y, -7);
+    assert.equal(box.max.x, 5);
+    assert.equal(box.max.y, 11);
+
+  });
+
+  it('can be expanded from by an array of points', function() {
+
+    var p1 = new Vec2(-1,-7);
+    var p2 = new Vec2(5,11);
+    var box1 = Box2.fromPoints([p1, p2]);
+    var box2 = box1.expandByPoints([new Vec2(0,0), new Vec2(-100, 20)]);
+
+    assert.equal(box2.min.x, -100);
+    assert.equal(box2.min.y, -7);
+    assert.equal(box2.max.x, 5);
+    assert.equal(box2.max.y, 20);
+
+  });
+
+  it('can test whether a point lies inside it', function() {
+
+    var p1 = new Vec2(0,0);
+    var p2 = new Vec2(100,100);
+    var box = Box2.fromPoints([p1, p2]);
+
+    assert.isTrue(box.isPointInside(new Vec2(0,0)));
+    assert.isTrue(box.isPointInside(new Vec2(100,0)));
+    assert.isTrue(box.isPointInside(new Vec2(50,0)));
+
+    assert.isFalse(box.isPointInside(new Vec2(-1,0)));
+    assert.isFalse(box.isPointInside(new Vec2(101,0)));
+
+  });
+
+});
